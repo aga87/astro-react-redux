b11a4c8a7414a60e839b97e510bbbf34d1bcd659
Aga <aga.labonarska@outlook.com>
2020-05-25 19:14:42 +0100

    replaced inline svg sizing with css

--- src/Components/Star.jsx
+++ src/Components/Star.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Star() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80" height="100%">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 80 80"
+      className="o-star"
+    >
       <polygon
         points="0,30 30,28 40,0 50,28 80,30 56,47 64,77 40,60 14,77 22,47 0,30"
         fill="yellow"
