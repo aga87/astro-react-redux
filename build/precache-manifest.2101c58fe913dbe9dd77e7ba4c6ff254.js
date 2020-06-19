5cdbfd66323f447796a1470810a56d9b95205a9e
Aga <aga.labonarska@outlook.com>
2020-06-19 12:10:19 +0100

    build

---
+++ build/precache-manifest.2101c58fe913dbe9dd77e7ba4c6ff254.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffd5f16918a70848e988c3ed2fd015f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3083e900d259b8cd8b9a",
+    "url": "/static/css/main.7f00367e.chunk.css"
+  },
+  {
+    "revision": "88a29ef5aa5c9f817be9",
+    "url": "/static/js/2.38f8b907.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.38f8b907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3083e900d259b8cd8b9a",
+    "url": "/static/js/main.2cc86340.chunk.js"
+  },
+  {
+    "revision": "ec4bde65fd3f8be9a9d0",
+    "url": "/static/js/runtime-main.0f635a3f.js"
+  },
+  {
+    "revision": "fdc4060f6e5225853514329b086ea924",
+    "url": "/static/media/guess.fdc4060f.mp3"
+  },
+  {
+    "revision": "3c40b1ad090fdb44ce1af11f399d187e",
+    "url": "/static/media/menu.3c40b1ad.wav"
+  },
+  {
+    "revision": "90dee89d467f1154f1608f2b3aa04f72",
+    "url": "/static/media/pass.90dee89d.mp3"
+  },
+  {
+    "revision": "26dce6d55a5936f8999728719343f516",
+    "url": "/static/media/start.26dce6d5.mp3"
+  },
+  {
+    "revision": "621cf67a07b404634cf35d0cea2ce41c",
+    "url": "/static/media/sunrise-from-space.621cf67a.jpg"
+  }
+]);
